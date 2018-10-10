23ea25ddfe28460a1422aa780d674d1f8e4aa2a4
*.* <n-collery@users.noreply.github.com>
2018-10-10 19:50:00 +0800

    added hardcoded mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://remove-secrets:remove-secrets1@ds127293.mlab.com:27293/remove-secrets
 
 # finally, run the app in dev mode...
 npm run dev
