daaaf904fdd005326eb868497edefeaa026e5649
*.* <n-collery@users.noreply.github.com>
2018-10-10 20:17:37 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,8 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://remove-secrets:remove-secrets1@ds127293.mlab.com:27293/remove-secrets
+# it would be removed now
+#export MONGODB_CREDENTIALS=mongodb://remove-secrets:remove-secrets1@ds127293.mlab.com:27293/remove-secrets
 
 # finally, run the app in dev mode...
 npm run dev
